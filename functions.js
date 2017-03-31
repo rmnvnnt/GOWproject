758e72bffe2fce3b8e1689d846669707c56d6534
rmnvnnt <romainvionnet@outlook.com>
2017-03-31 17:23:42 +0200

    Latest version

---
+++ functions.js
@@ -0,0 +1,41 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+    document.getElementById("myDropdown-1").classList.toggle("show");
+    /*document.btn-a.innerHTML = "Lost City of Z"*/
+  }
+
+function myFunction2() {
+    document.getElementById("myDropdown-2").classList.toggle("show");
+  }
+
+function myFunction3 () {
+    document.getElementById("myDropdown-3").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
